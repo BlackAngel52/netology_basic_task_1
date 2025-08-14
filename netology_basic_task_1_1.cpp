13260965f08f66a492b15ddd7ddb2babbc6082c0
BlackAngel <gfk.gfk@bk.ru>
2025-08-15 02:31:20 +0300

    Completed first task

---
+++ netology_basic_task_1_1.cpp
@@ -0,0 +1,83 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    enum class months
+    {
+            january = 1,
+            february,
+            march,
+            april,
+            may,
+            june,
+            july,
+            august,
+            september,
+            october,
+            november,
+            december
+    };
+
+
+    int number_of_month{ 1 };
+    while (number_of_month != 0)
+    {
+        std::cout << "Enter number of month or 0 if you want to exit: " << std::endl;
+        std::cin >> number_of_month;
+
+        months res = static_cast<months>(number_of_month);
+        switch (res)
+        {
+        case months::january:
+            std::cout << "January" << std::endl;
+            break;
+        case months::february:
+            std::cout << "February" << std::endl;
+            break;
+        case months::march:
+            std::cout << "March" << std::endl;
+            break;
+        case months::april:
+            std::cout << "April" << std::endl;
+            break;
+        case months::may:
+            std::cout << "May" << std::endl;
+            break;
+        case months::june:
+            std::cout << "June" << std::endl;
+            break;
+        case months::july:
+            std::cout << "July" << std::endl;
+            break;
+        case months::august:
+            std::cout << "August" << std::endl;
+            break;
+        case months::september:
+            std::cout << "September" << std::endl;
+            break;
+        case months::october:
+            std::cout << "October" << std::endl;
+            break;
+        case months::november:
+            std::cout << "November" << std::endl;
+            break;
+        case months::december:
+            std::cout << "December" << std::endl;
+            break;
+        default:
+            if (number_of_month != 0)
+            {
+                std::cout << "Wrong Number!" << std::endl;
+            }
+            else
+            {
+                std::cout << "Goodbye!" << std::endl;
+            }
+            break;
+        };
+    };
+    
+
+    return 0;
+}
