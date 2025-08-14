4e909053cbc4d870c4b71f1bae62ad20fd0ab6cf
BlackAngel <gfk.gfk@bk.ru>
2025-08-15 02:48:58 +0300

    Completed second task

---
+++ netology_basic_task_1_2.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+
+
+struct bank
+{
+    std::string name{};
+    double money_left{};
+    int account_number{};
+};
+
+
+void change_value(bank& data, double money)
+{
+    data.money_left = money;
+}
+
+int main()
+{
+    double money{};
+    bank user;
+    std::cout << "Enter your account number: " << std::endl;
+    std::cin >> user.account_number;
+    std::cout << "Enter your name: " << std::endl;
+    std::cin >> user.name;
+    std::cout << "Enter your money on account: " << std::endl;
+    std::cin >> user.money_left;
+    std::cout << "Enter your NEW money on account: " << std::endl;
+    std::cin >> money;
+
+    change_value(user, money);
+
+    std::cout << "Your account: " << user.name << ", " << user.account_number << ", " << user.money_left << std::endl;
+
+}
